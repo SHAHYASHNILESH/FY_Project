d002f6c7dd123600d184430f6b1659992a5c0273
SHAHYASHNILESH <nileshshah0409@yahoo.co.in>
2024-04-09 17:15:30 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import home
+import account, history
+
 # Define CSS styles for buttons and hover effects
 button_styles = """
     <style>
@@ -8,8 +10,9 @@ button_styles = """
             color: white;
             border: 2px solid orange;
             border-radius: 5px;
-            padding: 10px 20px;
-            font-size: 36px;
+            
+            margin-top:15px;
+            font-size: 10px;
             cursor: pointer;
             transition: background-color 0.3s, color 0.3s;
         }
@@ -19,7 +22,9 @@ button_styles = """
         }
     </style>
 """
-original_title = '<h1 style="font-family: serif; color:black; font-size: 36px;">Dashboard </h1>'
+original_title = (
+    '<h1 style="font-family: serif; color:black; font-size: 36px;margin-top:-50px;">Dashboard </h1>'
+)
 
 background_image = """
 <style>
@@ -31,6 +36,8 @@ background_image = """
 }
 </style>
 """
+
+
 # Main Streamlit app
 def main():
     st.markdown(background_image, unsafe_allow_html=True)
@@ -38,16 +45,43 @@ def main():
     st.markdown(original_title, unsafe_allow_html=True)
 
     # Buttons with text
-    col1, col2, col3 = st.columns(3)
+    col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
-        if st.button("Upload Pic", key="button1"):
-           home.main()
+        if st.button("Upload Image", key="button1"):
+            # Redirect to the home page
+            st.experimental_set_query_params(page="home")
+
     with col2:
-        if st.button("Prediciton", key="button2"):
-            st.write("Button 2 clicked!")
+        if st.button("Power Prediction", key="button2"):
+            # Redirect to the prediction page
+            st.experimental_set_query_params(page="prediction")
+
     with col3:
         if st.button("Govt-Schemes", key="button3"):
-            st.write("Button 3 clicked!")
+            # Redirect to the government schemes page
+            st.experimental_set_query_params(page="govt_schemes")
+    with col4:
+        if st.button("History", key="button4"):
+            # Redirect to the account page
+            st.experimental_set_query_params(page="history")
+
+    with col5:
+        if st.button("Login/Sign-up", key="button5"):
+            # Redirect to the account page
+            st.experimental_set_query_params(page="account")
+
+    # Retrieve the page parameter from the URL
+    page = st.experimental_get_query_params().get("page", [""])[0]
+
+    # Render different components based on the selected page
+    if page == "home":
+        home.main()
+    elif page == "account":
+        account.app()
+    elif page == "history":
+        history.main()
+    # Add more elif conditions for other pages
+
 
 if __name__ == "__main__":
     main()
